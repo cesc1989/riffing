ccb70f763da937338196fa762f69f1f291b04179
Francisco Quintero Coronell <frajaquico@yahoo.es>
2024-10-10 18:32:44 -0500

    Improve test on exceptions with sentry

--- exceptions.rb
+++ exceptions.rb
@@ -1,28 +1,45 @@
 # Prueba crear exceptiones personalizadas y cómo hacerlas interactuar con
 # ExceptionLogger
 
-class ExceptionLogger
-  def self.log(exception, log_message)
-    puts log_message
+require "amazing_print"
 
-    begin
-      raise exception, "✅ #{exception.message}", exception.backtrace
-    rescue exception.class => e
-      puts "Hola error"
-    end
+class BitlyErrorException < StandardError
+  def initialize(msg = 'Exception de Bitly')
+    super(msg)
   end
 end
 
-class BitlyErrorException < StandardError
-  def initialize(msg = 'soy una exception')
-    super(msg)
+class Sentry
+  def self.capture_exception(exception, level: "error", extra: {})
+    ap exception
+    ap extra
+  end
+end
+
+class ExceptionLogger
+  def self.log(exception, log_message, options = {})
+    puts log_message
+
+    Sentry.capture_exception(
+      exception,
+      extra: options
+    )
   end
 end
 
 class PruebaException
   def prueba_esta
-    ExceptionLogger.log(BitlyErrorException.new, 'soy un mensaje de error')
+    raise BitlyErrorException
+  rescue => e
+    ExceptionLogger.log(e, "Error de PruebaEsta")
+  end
+
+  def levanta_exception
+    raise BitlyErrorException
+  rescue => e
+    ExceptionLogger.log(e, "Levanté exception")
   end
 end
 
 PruebaException.new.prueba_esta
+PruebaException.new.levanta_exception

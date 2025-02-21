330a3c5dbfc7b72497aad7d787eb77074e69b5df
Francisco Quintero Coronell <frajaquico@yahoo.es>
2025-02-21 09:45:57 -0500

    Improve tests for to_fs vs to_s comparison for Rails 7.1

--- rails_labs/use_to_fs_instead_to_s.rb
+++ rails_labs/use_to_fs_instead_to_s.rb
@@ -21,18 +21,19 @@ Date::DATE_FORMATS[:default] = '%d/%m/%Y'
 class DateDeprecatedConversionsTest < Minitest::Test
   # Fails. #to_s does not accept an argument
   def test_to_s_conversion_with_argument
-    # assert_equal Date.new(2023, 6, 21).to_s(:prueba), "2023/06/21"
-
     assert_raises ArgumentError do
       Date.new(2023, 6, 21).to_s(:prueba)
     end
   end
 
-  # Starting Rails 7.1.4, use #to_fs to pass an argument
-  def test_to_fs_conversion_with_argument
-    assert_equal Date.new(2023, 6, 21).to_fs(:prueba), "2023/06/21"
+  # Fails. #to_s does not apply the default format.
+  def test_to_s_converstion_without_argument
+    refute_equal Date.new(2023, 6, 21).to_s, "2023/06/21"
+
+    assert_equal Date.new(2023, 6, 21).to_s, "2023-06-21"
   end
 
+  # Starting Rails 7.1.4, use #to_fs to pass an argument
   def test_default_format_with_to_fs
     assert_equal Date.new(2023, 6, 21).to_fs, "21/06/2023"
     assert_equal Date.new(2023, 6, 21).to_fs(:default), "21/06/2023"
